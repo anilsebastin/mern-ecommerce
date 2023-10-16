af90a864587b149387c094fdd958232994126e00
anilsebastin <anilsebastin@gmail.com>
2023-10-16 14:44:23 +0530

    Created Login, Signup page with react routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Singup from "../features/auth/components/Singup";
+
+function SignupPage() {
+  return (
+    <div>
+      <Singup />
+    </div>
+  );
+}
+
+export default SignupPage;
