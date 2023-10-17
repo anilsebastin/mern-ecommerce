5b4c78f3407f2f93ca47478d4a55153cec33092d
anilsebastin <anilsebastin@gmail.com>
2023-10-17 11:46:16 +0530

    Checkout, ProductDetail page added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import ProductDetail from "./features/product/components/ProductDetail";
 import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 import SignupPage from "./pages/SignupPage";
 
 import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
@@ -21,6 +24,14 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "/product-detail",
+    element: <ProductDetailPage />,
   },
 ]);
 
